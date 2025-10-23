36f1c114d9f89a67b4deb666384d91840d5efda1
Sebbbenator <eaa25ssg@students.eaaa.dk>
2025-10-23 11:30:14 +0200

    opgave 2-3-4 lavet

--- 03/js/script.js
+++ 03/js/script.js
@@ -1,30 +1,33 @@
-"use strict"
-
+"use strict";
 
 const body = document.body;
 
-
 function updateBackground() {
-const width = window.innerWidth;
-    if (width < 640){
-        body.style.background = "purple";
-    }
-    else if (width < 760){
-        body.style.background = "yellow";
-    }
-    else if (width < 900){
-        body.style.background = "blue"; 
-    }
-    else if (width < 1100){
-        body.style.background = "red"; 
-    }
-     else if (width < 1300){
-        body.style.background = "green"; 
-    }
-    else {
-        body.style.background = "lightgray"; 
-    }
+  const width = window.innerWidth;
+  if (width < 640) {
+    body.style.background = "lightblue";
+  }
+  if (width < 640) {
+    body.style.background = "purple";
+  } else if (width < 760) {
+    body.style.background = "yellow";
+  } else if (width < 900) {
+    body.style.background = "blue";
+  } else if (width < 960) {
+    body.style.background = "orange";
+  } else if (width < 1100) {
+    body.style.background = "red";
+    } else if (width < 1200) {
+    body.style.background = "pink";
+  } else if (width < 1300) {
+    body.style.background = "green";
+  } else {
+    body.style.background = "lightgray";
+  }
 }
+
+// 480, 760, 960, 1200
+
 // Kør ved load
 updateBackground();
 
--- 04/js/script.js
+++ 04/js/script.js
@@ -1,35 +1,39 @@
 "use strict";
 
-const getNumber1Field = document.getElementById("number1")
-const getNumber2Field = document.getElementById("number2")
+const getNumber1Field = document.getElementById("number1");
+const getNumber2Field = document.getElementById("number2");
 const resultAdditionField = document.getElementById("resultAddition");
 const additionBtn = document.getElementById("addNumbersBtn");
 
 additionBtn.addEventListener("click", () => {
-  
-    const number1 = Number(getNumber1Field.value);
-    const number2 = Number(getNumber2Field.value);
-    const result = number1 + number2
-    resultAdditionField.value = result;
-
+  const number1 = Number(getNumber1Field.value);
+  const number2 = Number(getNumber2Field.value);
+  const result = number1 + number2;
+  resultAdditionField.value = result;
 });
 
-
-const getNumber3Field = document.getElementById("number3")
-const getNumber4Field = document.getElementById("number4")
+const getNumber3Field = document.getElementById("number3");
+const getNumber4Field = document.getElementById("number4");
 const resultSubstractionField = document.getElementById("resultSubstraction");
 const substractBtn = document.getElementById("substractNumbersBtn");
 
 substractBtn.addEventListener("click", () => {
-  
-    const number3 = Number(getNumber3Field.value);
-    const number4 = Number(getNumber4Field.value);
-    const result = number3 - number4
-    resultSubstractionField.value = result;
-
+  const number3 = Number(getNumber3Field.value);
+  const number4 = Number(getNumber4Field.value);
+  const result = number3 - number4;
+  resultSubstractionField.value = result;
 });
 
+const getNumber5Field = document.getElementById("number5");
+const getNumber6Field = document.getElementById("number6");
+const resultMultiplicationField = document.getElementById(
+  "resultMultiplication"
+);
+const multiplyBtn = document.getElementById("multiplyNumbersBtn");
 
-
-
-
+multiplyBtn.addEventListener("click", () => {
+  const number5 = Number(getNumber5Field.value);
+  const number6 = Number(getNumber6Field.value);
+  const result = number5 * number6;
+  resultMultiplicationField.value = result;
+});
